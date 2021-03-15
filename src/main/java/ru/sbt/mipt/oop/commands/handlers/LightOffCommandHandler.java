d7b95a697f2575c0850a1a8492e71e4fb337fc4c
tanya <tanya@localhost.localdomain>
2021-03-15 23:53:24 +0300

    done

--- src/main/java/ru/sbt/mipt/oop/commands/handlers/LightOffCommandHandler.java
+++ src/main/java/ru/sbt/mipt/oop/commands/handlers/LightOffCommandHandler.java
@@ -5,6 +5,7 @@ import ru.sbt.mipt.oop.commands.SensorCommand;
 import ru.sbt.mipt.oop.home.Light;
 import ru.sbt.mipt.oop.home.Room;
 import ru.sbt.mipt.oop.home.SmartHome;
+import ru.sbt.mipt.oop.home.action.*;
 import ru.sbt.mipt.oop.home.iterator.LightInRoomIterator;
 import ru.sbt.mipt.oop.home.iterator.SmartHomeSmartIterator;
 
@@ -17,19 +18,8 @@ public class LightOffCommandHandler implements CommandHandler {
 
     @Override
     public void doAction(SensorCommand command) {
-        if (command.getType() == CommandType.LIGHT_OFF) {
-            SmartHomeSmartIterator smartHomeIterator = smartHome.createIterator();
-            while (smartHomeIterator.hasMore()) {
-                Room room = smartHomeIterator.getNext();
-                LightInRoomIterator lightIterator = room.createLightInRoomIterator();
-                while (lightIterator.hasMore()) {
-                    Light light = lightIterator.getNext();
-                    if (light.getId().equals(command.getObjectId())) {
-                        light.setOn(false);
-                        System.out.println("Pretent we're sending command " + command);
-                    }
-                }
-            }
-        }
+        System.out.println("Pretent we're sending command " + command);
+        Action action = new Action(ActionType.LIGHT_OFF);
+        smartHome.execute(action);
     }
 }
--- src/main/java/ru/sbt/mipt/oop/home/action/Actionable.java
+++ src/main/java/ru/sbt/mipt/oop/home/action/Actionable.java
@@ -1,7 +1,5 @@
 package ru.sbt.mipt.oop.home.action;
 
-import ru.sbt.mipt.oop.home.action.Action;
-
 public interface Actionable {
     void execute(Action action);
 }
